a327f91bc936dbfba441698ab23f668c3f94bcad
alquantumai <owaisibnmushtaq@gmail.com>
2024-02-16 08:34:04 +0530

    setuptools version and pip version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 REPO_NAME = "PythonPackage"
 PKG_NAME= "mongo_connection_owais"
 AUTHOR_USER_NAME = "HakimOwais"
